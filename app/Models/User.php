3633cd8043405cd12d813fa4df2071437f433fa0
Ezzara <estusaeora@gmail.com>
2026-01-19 02:37:19 +0700

    fix api auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,11 +11,14 @@ use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 
+// api authentication required
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    use HasApiTokens, Notifiable;
     /**
      * The attributes that are mass assignable.
      *
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 // Book Controller
 use App\Http\Controllers\Api\BookController; 
-
+// Auth Facade
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+// API login route
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+
+    if (!Auth::attempt($credentials)) {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    $token = $request->user()->createToken('postman')->plainTextToken;
+
+    return response()->json(['token' => $token]);
+});
+
 
 Route::apiResource('books', BookController::class);
